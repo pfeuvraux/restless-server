b4f1ed62e31893e6d07d3d928d2914ba08b3bf07
Paul Feuvraux <pfeuvraux@gmail.com>
2021-04-14 20:47:30 +0200

    Various improvements

--- app/models/db/__init__.py
+++ app/models/db/__init__.py
@@ -1,1 +1,16 @@
-# //init DB connection
+# //Init DB session
+
+from . import (
+  auth
+)
+
+from cassandra.cluster import Cluster
+from app.config import default_settings
+
+__cluster = Cluster(
+  default_settings['database']['hosts'],
+  port=default_settings['database']['port']
+)
+scylladb = __cluster.connect(
+  default_settings['database']['keyspace']
+)
