c4804440b73831e7fa0306b11ef661cafc8d4649
Paul Feuvraux <pfeuvraux@gmail.com>
2021-04-16 10:18:30 +0200

    create_keyspace sample

--- tools/db/create_keyspace.py
+++ tools/db/create_keyspace.py
@@ -1,1 +1,12 @@
 # FIXME: ARGPARSE + CREATE KEYSPACE + SLEEP BEFORE ANYTHING
+
+from cassandra.cluster import Cluster
+
+cluster = Cluster(['172.18.0.2'])
+session = cluster.connect()
+
+keyspace_name = "restless"
+
+session.execute(
+  f"CREATE KEYSPACE IF NOT EXISTS {keyspace_name} WITH replication = {{'class': 'SimpleStrategy', 'replication_factor': '1'}};"
+)
